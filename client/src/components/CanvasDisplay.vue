d4ce6481da2772210985d7b9b87aa99d8348b9a0
Smallyellowface <jctheathlete@gmail.com>
2019-08-12 21:42:36 -0700

    added canvas diaplay to tableservice

---
+++ client/src/components/CanvasDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-container fluid>
+      <b-row>
+        <b-col></b-col>
+        <b-col>
+          <canvas id="myCanvas" width="1000" height="800"></canvas>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+<script>
+export default {
+  name: "CanvasDisplay",
+  methods: {
+    getCoordMouse(event) {
+      console.log(event.clientX); // x coordinate
+      console.log(event.clientY); // y coordinate
+    }
+  }
+};
+</script>
+<style>
+#myCanvas {
+  border: 1px solid #000000;
+  background-color: #32a852;
+}
+</style>
+
--- client/src/views/TableService.vue
+++ client/src/views/TableService.vue
@@ -1,16 +1,42 @@
 <template>
-  <div></div>
+  <div>
+<FreeTransform 
+    :x="0"
+    :y="0"
+    :scale-x="1"
+    :scale-y="1"
+    :width="100"
+    :height="100"
+    :angle="0"
+    :offset-x="0"
+    :offset-y="0"
+    @update="update($event)"
+>
+    <div class="element" :style="{width: `100px`,height: `100px`}">
+        <img src="..."/>
+    </div>
+
+</FreeTransform>
+    <CanvasDisplay />
+  </div>
 </template>
 <script>
+import FreeTransform from '../../node_modules/vue-free-transform'
+import CanvasDisplay from "@/components/CanvasDisplay.vue";
 import { EventBus } from "@/event-bus.js";
 export default {
   name: "TableService",
-  beforeMount(){
+  mounted() {
     EventBus.$emit("sent", this.labelMenu);
+  },
+  components: {
+    CanvasDisplay,
+    FreeTransform
   },
   data() {
     return {
-      labelMenu: "Table Service"
+      labelMenu: "Table Service",
+    
     };
   }
 };
